f1fa81abb0c004195d38c696dce6939122fae47a
ing <inyoman_gurnitha@yahoo.com>
2020-05-10 00:38:16 +0700

    5.1. Working with Models & Media files

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Contact model
+class Contact(models.Model):
+	name		= models.CharField(max_length=30)
+	email		= models.CharField(max_length=100)
+	phone		= models.IntegerField()
+	info		= models.CharField(max_length=30)
+	gender		= models.CharField(max_length=30, choices=(
+							('male', 'Male'),
+							('female', 'Female'),
+							('other', 'Other')))
+	image		= models.ImageField(upload_to='images/', blank=True)
+	date_added 	= models.DateField(auto_now_add=True)
