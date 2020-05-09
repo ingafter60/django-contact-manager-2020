d1edc8eef1033d51c75c97c29a4b90f4ee7abcfc
ing <inyoman_gurnitha@yahoo.com>
2020-05-09 22:46:09 +0700

    4.1. (Create Home and About pages) Understanding MVT Layers

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+	return render(request, 'index.html')
+
+def aboutPage(request):
+	return render(request, 'about.html')
