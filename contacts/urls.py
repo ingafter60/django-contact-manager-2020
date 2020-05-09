15be1516901813a2649c88dbc9e9bbe080c672fb
ing <inyoman_gurnitha@yahoo.com>
2020-05-09 23:06:20 +0700

    4.3. Loading static files

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='home'),
-	path('about/', views.aboutPage, name='about'),
+	path('', views.home, name='home')
 ]
