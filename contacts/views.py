24d46cabb7a72910ff78059c97ca68880cfc8064
ing <inyoman_gurnitha@yahoo.com>
2020-05-10 08:36:38 +0700

    6.3. Passing contacts objects to template

--- contacts/views.py
+++ contacts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 def home(request):
 	context = {
-		'status': 'Working on this project',
-		'age': 63,
+		'contacts': Contact.objects.all(),
 	}
 	return render(request, 'index.html', context)
 
