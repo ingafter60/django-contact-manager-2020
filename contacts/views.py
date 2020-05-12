c7012fc2d9994d9d14b0bebd022a7b81d3fcdc78
ing <inyoman_gurnitha@yahoo.com>
2020-05-12 11:16:21 +0700

    8.2. DetailView for contact details

--- contacts/views.py
+++ contacts/views.py
@@ -9,11 +9,11 @@ from .models import Contact
 # 	}
 # 	return render(request, 'index.html', context)
 
-def detail(request, id):
-	context = {
-		'contact': get_object_or_404(Contact, pk=id)
-	}
-	return render(request, 'detail.html', context)
+# def detail(request, id):
+# 	context = {
+# 		'contact': get_object_or_404(Contact, pk=id)
+# 	}
+# 	return render(request, 'detail.html', context)
 
 
 # Class based-views
@@ -21,3 +21,8 @@ class HomePageView(ListView):
 	template_name = 'index.html'
 	model 		  = Contact
 	context_object_name = 'contacts'
+
+class ContactDetailView(DetailView):
+	template_name = 'detail.html'
+	model 		  = Contact
+	context_object_name = 'contact'
